871324d28fcc377e0b0425cbbf5d948005afa1da
romankupriianov <romankupriianov4@yandex.ru>
2024-10-08 23:12:05 +0400

    separating standard library imports and third party imports

--- msdt-1/lab1.py
+++ msdt-1/lab1.py
@@ -1,5 +1,6 @@
 import sys
 import random
+
 from PyQt5.QtWidgets import QMainWindow, QFrame, QDesktopWidget, QApplication
 from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
 from PyQt5.QtGui import QPainter, QColor
